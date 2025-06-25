04cd69577bcacdf3fb60d7ba9a5b6eab98e0eb2d
tiwari786 <vipultiwari479@gmail.com>
2025-06-25 15:16:23 +0530

    Initial Commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -17,7 +17,9 @@ export default function App() {
   }, [])
 
   async function reviewCode() {
-    const response = await axios.post("http://localhost:3000/ai/get-review", { code })
+    const response = await
+      // axios.post("http://localhost:3000/ai/get-review", { code })
+      axios.post(`${import.meta.env.VITE_API_URL}/ai/get-review`, { code })
     setReview(response.data)
   }
 
